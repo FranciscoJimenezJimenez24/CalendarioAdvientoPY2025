39fc2430808dee3543ec35a6b4091ec3fc29eaa4
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2026-01-28 16:46:20 +0100

    dia 4 actualizado

--- dia4.py
+++ dia4.py
@@ -1,25 +1,26 @@
 def decode_santa_pin(code: str) -> str:
   lastNumber = ""
   pin = ""
-  codeArray = code.replace("[", "").split("]")
+  codeArray = code.split("]")
   codeArray.pop()
   if len(codeArray) != 4: return None
   for i in range(len(codeArray)):
-    if (len(codeArray[i]) > 1):
+    block = codeArray[i].replace("[","")
+    if (len(block) > 1):
       digit = 0
-      if (codeArray[i][1] == "+"):
-        digit = int(codeArray[i][0]) + 1*(len(codeArray[i])-1)
+      if (block[1] == "+"):
+        digit = int(block[0]) + 1*(len(block)-1)
       else:
-        digit = int(codeArray[i][0]) - 1*(len(codeArray[i])-1)
+        digit = int(block[0]) - 1*(len(block)-1)
       digit = digit % 10
       lastNumber = f"{digit}"
       pin += lastNumber
     else:
-      if (codeArray[i][0] == "<"): 
+      if (block[0] == "<"): 
         pin += lastNumber
       else:
-        pin += f"{codeArray[i][0]}"
-        lastNumber = codeArray[i][0]
+        pin += f"{block[0]}"
+        lastNumber = block[0]
   return pin
 
 text = decode_santa_pin('[9+][<][<][<]')
