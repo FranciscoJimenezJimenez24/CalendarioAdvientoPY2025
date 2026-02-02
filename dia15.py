f09fc13ed416e411916bd8d86057a1af95290878
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2026-02-02 19:40:36 +0100

    dia 15, 4 estrellas

---
+++ dia15.py
@@ -0,0 +1,53 @@
+def draw_table(data: list[dict[str, str | int]], sortBy: str) -> str:
+  # order the data
+  dataSorted = sorted(data, key=lambda listElement:listElement[sortBy])
+
+  # the length of every column
+  maxLengthColumn = []
+  for i in range(len(dataSorted)):
+    for j, (clave, valor) in enumerate(dataSorted[i].items()):
+      if len(maxLengthColumn) < len(dataSorted[0]):
+        maxLengthColumn.append(len(str(valor)))
+      else:
+        if (maxLengthColumn[j] < len(str(valor))):
+          maxLengthColumn[j] = len(str(valor))
+  
+  # the border of the table
+  borderTable = "+"
+  for i in range(len(maxLengthColumn)):
+    borderTable += "-"*(maxLengthColumn[i] + 2) + "+"
+  
+  # the header of the table
+  header = borderTable + "\n|"
+  for i in range(len(maxLengthColumn)):
+    header += " " + chr((i+1) + 64) + " "*(maxLengthColumn[i] - 1) + "|"
+  header += "\n" + borderTable
+
+  # the structure of the table
+  table = header + "\n"
+  for i in range(len(dataSorted)):
+    table += "|"
+    for j, (clave, valor) in enumerate(dataSorted[i].items()):
+      table += " " + valor + " "*(maxLengthColumn[j] - len(valor)) + "|"
+    table += "\n"
+  return table + borderTable
+  
+
+
+print(draw_table(
+  [
+    { "gift": 'Book', "quantity": 5 },
+    { "gift": 'Music CD', "quantity": 1 },
+    { "gift": 'Doll', "quantity": 10 }
+  ],
+  'quantity'
+))
+
+print(draw_table(
+  [
+    { "name": 'Charlie', "city": 'New York' },
+    { "name": 'Alice', "city": 'London' },
+    { "name": 'Bob', "city": 'Paris' }
+  ],
+  'name'
+))
