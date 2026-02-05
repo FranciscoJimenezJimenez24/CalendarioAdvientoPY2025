abdf3a3ff157fe1d094c609e73372718d69568bf
FranciscoJimenezJimenez24 <146998590+FranciscoJimenezJimenez24@users.noreply.github.com>
2026-02-05 16:40:04 +0100

    dia 6, 4 estrellas

--- dia6.py
+++ dia6.py
@@ -1,38 +1,24 @@
 from typing import List, Dict
 
-# def match_gloves(gloves: List[Dict[str, str]]) -> List[str]:
-#   pairs = []
-#   colours = []
-#   for glove in gloves:
-#     colours.append(glove["color"])
-#   colours = list(set(colours))
-#   matches = list(set(colours))
-#   for glove in gloves:
-#     for i in range (len(colours)):
-#       if (colours[i] == glove["color"]):
-#         matches[i] += glove["hand"]
-#   for i in range (len(matches)):
-#     left = matches[i].count("L")
-#     right = matches[i].count("R")
-#     if (left != 0 and right != 0):
-#       j = 0
-#       minus = ""
-#       if (left > right): minus = right
-#       elif (right > left): minus = left
-#       else: minus = right
-#       while (j < minus):
-#         pairs.append(matches[i][:-(left+right)])
-#         j+=1
-#   return pairs
-
 def match_gloves(gloves: List[Dict[str, str]]) -> List[str]:
   pairs = []
+  pendientes = []
+
   for glove in gloves:
-    for glovePosition in gloves:
-      if glovePosition["hand"] != glove["hand"] and glovePosition["color"] == glove["color"]:
+    encontrado = False
+    
+    for posible_pareja in pendientes:
+      if (posible_pareja["color"] == glove["color"] and 
+        posible_pareja["hand"] != glove["hand"]):
+        
         pairs.append(glove["color"])
-        gloves.remove(glove)
-        gloves.remove(glovePosition)
+        pendientes.remove(posible_pareja)
+        encontrado = True
+        break 
+      
+    if not encontrado:
+      pendientes.append(glove)
+          
   return pairs
 
 gloves = [
